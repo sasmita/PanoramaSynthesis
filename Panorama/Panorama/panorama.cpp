2f235972e91b5213049a913e846bf51d59ad26d6
sasmita <spatra@pdx.edu>
2014-03-10 23:57:43 -0700

    Adding rest of the functionalities

--- Panorama/Panorama/panorama.cpp
+++ Panorama/Panorama/panorama.cpp
@@ -18,13 +18,6 @@ using namespace std;
 using namespace cv;
 using namespace cv::detail;
 
-unsigned int numImages = 0;
-
-double scale = 0;
-double sscale = 0;
-double aspect = 0;
-double warpedImgScale = 0;
-
 vector <string> imageNames;
 vector <ImageFeatures> features;
 vector <Mat> images;
@@ -33,7 +26,23 @@ vector <MatchesInfo> pairwiseMatches;
 vector <int> indices;
 vector <CameraParams> cameras;
 vector <Size> sizes;
+vector <Point> corners;
+vector <Mat> masksWarped;
+vector <Mat> imagesWarped;
+vector <Mat> masks;
+vector <Mat> imagesWarpedFloat;
 
+unsigned int numImages = 0;
+
+double scale = 0;
+double sscale = 0;
+double aspect = 0;
+double warpedImgScale = 0;
+
+int blendType = Blender::FEATHER;
+
+Ptr <RotationWarper> warper;
+Ptr <ExposureCompensator> compensator;
 
 void setup()
 {
@@ -178,6 +187,199 @@ void computeWarpedImageScale(void)
 	warpedImgScale = medianFocal * 0.5;
 }
 
+void straightening(void)
+{
+	cout << "In straightening.." << endl;
+	
+	vector <Mat> rmats;
+
+	for(unsigned int i = 0; i < cameras.size(); i++)
+	{
+		rmats.push_back(cameras[i].R);
+	}
+
+	waveCorrect(rmats, WAVE_CORRECT_HORIZ);
+
+	for(unsigned int i = 0; i < cameras.size(); i++)
+	{
+		cameras[i].R = rmats[i];
+	}
+}
+
+void warpingImages(void)
+{
+	cout << "In warping images.." << endl;
+
+	corners.resize(numImages);	 
+	masksWarped.resize(numImages);
+	imagesWarped.resize(numImages);
+	sizes.resize(numImages);
+	vector <Mat> masks(numImages);
+
+	for(unsigned int i = 0; i < numImages; i++)
+	{
+		masks[i].create(images[i].size(), CV_8U);
+		masks[i].setTo(Scalar::all(255));
+	}
+
+	warper = new SphericalWarper(static_cast<float>(aspect*warpedImgScale));
+
+	for(unsigned int i = 0; i < numImages; i++)
+	{
+		Mat_<float> K;
+		cameras[i].K().convertTo(K, CV_32F);
+		K(0,0) *= (float) aspect; K(0,2) *= (float) aspect;
+		K(1,1) *= (float) aspect; K(1,2) *= (float) aspect;
+
+		corners[i] = warper->warp(images[i], K, cameras[i].R, INTER_LINEAR, BORDER_REFLECT, imagesWarped[i]);
+		sizes[i] = imagesWarped[i].size();
+
+		warper->warp(masks[i], K, cameras[i].R, INTER_NEAREST, BORDER_CONSTANT, masksWarped[i]);
+	}
+
+	imagesWarpedFloat.resize(numImages);
+
+	for(unsigned int i = 0; i < numImages; i++)
+	{
+		imagesWarped[i].convertTo(imagesWarpedFloat[i], CV_32F);
+	}
+}
+
+void ExposureCompensation(void)
+{
+	cout << "In exposure compensation .." << endl;
+
+	compensator = ExposureCompensator::createDefault(ExposureCompensator::GAIN_BLOCKS);
+	compensator->feed(corners, imagesWarped, masksWarped);
+}
+
+void findingSeamMasks(void)
+{
+	cout << " In finding seam .." << endl;
+
+	Ptr <SeamFinder> seamFinder = new GraphCutSeamFinder(GraphCutSeamFinderBase::COST_COLOR);
+	seamFinder->find(imagesWarpedFloat, corners, masksWarped);
+
+	images.clear();
+	imagesWarped.clear();
+	imagesWarpedFloat.clear();
+}
+
+void compositingImages(void)
+{
+	cout << "In compositing mages .." << endl;
+
+	Mat img, img2, imgWarped, imgWarpedS;
+	Mat dilatedMask, seamMask, mask, maskWarped;
+	Ptr <Blender> blender;
+	bool isComposeScaleSet = false;
+	double composeScale = 0.0;
+
+	RotationWarper* warper;
+
+	for (unsigned int i = 0; i < numImages; i++)
+	{
+		cout << "\t compositing image" << indices[i] << endl;
+
+		img = imread(imageNames[i]);
+
+		if(!isComposeScaleSet)
+		{
+			isComposeScaleSet = true;
+
+			composeScale = 1.0;
+			double composeWorkAspect = composeScale / scale;
+
+			warpedImgScale *= composeWorkAspect;
+
+			warper = new SphericalWarper(static_cast<float>(warpedImgScale));
+
+			//update corners and sizes
+			for(unsigned int k = 0; k < numImages; k++)
+			{
+				cameras[k].focal *= composeWorkAspect;
+				cameras[k].ppx *= composeWorkAspect;
+				cameras[k].ppy *= composeWorkAspect;
+
+				Size sz = orgImagesizes[k];
+
+				if(std::abs(composeScale - 1) > 1e-1)
+				{
+					sz.width = cvRound(orgImagesizes[k].width * composeScale);
+					sz.height = cvRound(orgImagesizes[k].height * composeScale);
+				}
+
+				Mat K;
+				cameras[k].K().convertTo(K, CV_32F);
+				Rect roi = warper->warpRoi(sz, K, cameras[k].R);
+				corners[k] = roi.tl();
+				sizes[k] = roi.size();
+			}
+		}
+		if (abs(composeScale - 1) > 1e-1)
+			cv::resize(img, img2, Size(), composeScale, composeScale);
+		else
+			img2 = img;
+
+		img.release();
+		Size img_size= img2.size();
+		
+		Mat K;
+        cameras[i].K().convertTo(K, CV_32F);
+
+		warper->warp(img2, K, cameras[i].R, INTER_LINEAR, BORDER_REFLECT, imgWarped);
+
+		mask.create(img_size, CV_8U);
+		mask.setTo(Scalar::all(255));
+
+		//Warp the current image mask
+		warper->warp(mask, K, cameras[i].R, INTER_LINEAR, BORDER_CONSTANT, maskWarped);
+
+		//Compensate exposure
+		compensator->apply(i, corners[i], imgWarped, maskWarped);
+
+		imgWarped.convertTo(imgWarpedS, CV_16S);
+		imgWarped.release();
+		img2.release();
+		mask.release();
+
+		cv::dilate(masksWarped[i], dilatedMask, Mat());
+		cv::resize(dilatedMask, seamMask, maskWarped.size());
+		maskWarped = seamMask & maskWarped;
+
+		if (blender.empty())
+		{
+			blender = Blender::createDefault(blendType, 0);
+			Size dst_sz = resultRoi(corners,sizes).size();
+			float blend_width = sqrt(static_cast<float>(dst_sz.area())) * 5 / 100.f;
+			if (blend_width < 1.f)
+			{
+				blender = Blender::createDefault(Blender::NO, 0);
+				cout << "No blending " << endl;
+			}
+			else if(blendType == Blender::MULTI_BAND)
+			{
+				MultiBandBlender* mb = dynamic_cast<MultiBandBlender*>(static_cast<Blender*>(blender));
+				mb->setNumBands(static_cast<int>(ceil(log(blend_width)/log(2.)) - 1.));
+				cout << "MULTI_BAND " << mb->numBands() << endl;
+			}
+			else if(blendType == Blender::FEATHER)
+			{
+				FeatherBlender* fb = dynamic_cast<FeatherBlender*>(static_cast<Blender*>(blender));
+				fb->setSharpness(1.f/blend_width);
+				cout << "FEATHER " << fb->sharpness() << endl;
+			}
+			blender->prepare(corners, sizes);
+		}
+		//Blend the current image
+		blender->feed(imgWarpedS,maskWarped, corners[i]);
+	}
+	Mat result, result_mask;
+	blender->blend(result, result_mask);
+
+	imwrite("output.jpg", result);
+}
+
 int main()
 {
 	setup();
@@ -191,6 +393,14 @@ int main()
 	bundleAdjustment();
 
 	computeWarpedImageScale();
+
+    straightening();
+
+	warpingImages();
+
+	ExposureCompensation();
+
+	compositingImages();
 
 	getchar();
 	return 0;
